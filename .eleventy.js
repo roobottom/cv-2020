a552afccaefa5f7212626c8114f189470437efe0
Jon Roobottom <roo@roodesign.co.uk>
2020-02-03 16:57:21 +0000

    stubbing out pages

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (config) {
+  return {
+    dir: {
+      input: "_source",
+      includes: "_includes",
+      layouts: "_layouts",
+      dataTemplateEngine: "njk",
+      markdownTemplateEngine: "njk",
+      htmlTemplateEngine: "njk",
+      templateFormats: ["html", "njk"]
+    }
+  }
+}
