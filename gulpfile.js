6c5da77515b50c4b6ea03ff345b6f25faff22d5a
Jon Roobottom <roo@roodesign.co.uk>
2020-02-16 22:49:58 +0000

    Build process for pdf styles is working well

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,30 @@ const prince = require('prince')
 const util = require('util')
 
 const css = () => {
-  return src('./_source/_less/styles.less')
+  return src(['./_source/_less/styles.less', './_source/_less/pdf-styles.less'])
     .pipe(less())
     .pipe(dest('./_site/assets'))
 }
 
-const pdf = () => {
+const pdf = (callback) => {
   prince()
     .inputs('./_site/index.html')
     .output('./_site/jon-roobottom-cv.pdf')
+    .option('style', './_site/assets/pdf-styles.css')
     .execute()
     .then(function () {
       console.log('PDF generated OK')
     }, function() {
         console.log('PDF generation failed', util.inspect(error))
     })
+  callback()
 }
 
-
-exports.default = (callback) => {
-  watch('./_source/_less/**/*.less', { ignoreInitial: false }, css)
-  watch('./_site/index.html', pdf)
+exports.default = function(callback) {
+  console.log('watching')
+  watch(['./_site/index.html', './_source/_less/**/*.less'], 
+    { ignoreInitial: false }, 
+    series(css, pdf))
   callback()
 }
 exports.build = (callback) => {
