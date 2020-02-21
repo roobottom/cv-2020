efed6075f7b33b94908c68a04bc7acc92b25c57d
Jon Roobottom <roo@roodesign.co.uk>
2020-02-21 23:01:29 +0000

    styling and content updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,25 @@
 const markdownIt = require("markdown-it")
+const markdownItAttrs = require("markdown-it-attrs")
+const markdownItDiv = require("markdown-it-div")
+
 
 module.exports = function (config) {
 
-  //markdown
+  //markdown-it config
   let mdOptions = {
     typographer: true,
     quotes: '“”‘’',
     html: true
   }
-  let md = markdownIt(mdOptions)
-  config.setLibrary("md", md)
+  let md =  markdownIt(mdOptions)
+            .use(markdownItAttrs)
+            .use(markdownItDiv)
 
+  //11ty md config
+  config.setLibrary("md", md)
   config.addPairedShortcode("markdown", function(content) {
     return md.render(content)
   })
-
-
-
 
   //passthough
   config.addPassthroughCopy("_source/assets")
