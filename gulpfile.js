d50e18a64435fdc8d0bf5c12277bac79b982337b
Jon Roobottom <roobottom@Roodesign-Macbook-Spare.local>
2020-03-02 16:48:04 +0000

    updated build process

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,21 @@ const { src, dest, watch, series } = require('gulp')
 const less = require('gulp-less')
 const prince = require('prince')
 const util = require('util')
+const cp = require('child_process')
 
 const css = () => {
   return src(['./_source/_less/styles.less', './_source/_less/pdf-styles.less'])
     .pipe(less())
     .pipe(dest('./_site/assets'))
+}
+
+const eleventyLocal = (callback) => {
+  cp.spawn("npx", ["eleventy", '--serve'], { stdio: "inherit" })
+  callback()
+}
+const eleventy = (callback) => {
+  cp.spawn("npx", ["eleventy", ''], { stdio: "inherit" })
+  callback()
 }
 
 const pdf = (callback) => {
@@ -27,10 +37,7 @@ exports.default = function(callback) {
   console.log('watching')
   watch(['./_site/index.html', './_source/_less/**/*.less'], 
     { ignoreInitial: false }, 
-    series(css, pdf))
+    series(eleventyLocal, css, pdf))
   callback()
 }
-exports.build = (callback) => {
-  series(css, pdf)
-  callback()
-}
+exports.build = series(eleventy, css, pdf)
